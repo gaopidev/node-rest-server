833e5c225719e5a9993606a9b3dbd57fbd749705
gaopidev <gaop.developer.mx@gmail.com>
2020-01-21 01:22:19 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    //urlDB = 'mongodb://gaopidev:aKfLhbLFSYqGavIf@cafe-vfd7c.mongodb.net/test';
     urlDB = proces.env.MONGO_URI;
 }
 
