a92297d7397be3d2409ce7036fbe2ad83554476b
gaopidev <gaop.developer.mx@gmail.com>
2020-01-21 00:54:22 -0600

    RESTserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
-//Puerto
+// Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    //urlDB = 'mongodb://gaopidev:aKfLhbLFSYqGavIf@cafe-vfd7c.mongodb.net/test';
+    urlDB = 'mongodb+srv://gaopidev:aKfLhbLFSYqGavIf@cafe-vfd7c.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,24 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(require('./rutas/usuario'));
 
+//Conexion a mongoose
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
 
-app.get('/usuario', (req, res) => res.json('get Usuario'));
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+    console.log('Conectado a base de datos');
 });
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-
-});
-app.delete('/usuario', (req, res) => res.json('delete Usuario'));
 
 app.listen(process.env.PORT, () => console.log('Escuchando el puerto ', process.env.PORT));
