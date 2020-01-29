7034e3b557cd80d60afd257b56d3205ed9963264
gaopidev <gaop.developer.mx@gmail.com>
2020-01-28 23:51:01 -0600

    Autenticacion con Google

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Habilitar carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //Rutas
 app.use(require('./rutas/index'));
